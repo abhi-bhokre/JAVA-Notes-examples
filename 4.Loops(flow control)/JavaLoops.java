c2bfa5c1906cf92b1d569d041b727e318b0f628b
abhay bhokre <abhaybhokre00@gmail.com>
2025-01-15 21:41:20 +0530

    javaPattern

--- 4.Loops(flow control)/JavaLoops.java
+++ 4.Loops(flow control)/JavaLoops.java
@@ -5,29 +5,26 @@ import java.util.*;
 public class JavaLoops{
     public static void main(String args[]){
         Scanner inp = new Scanner(System.in);
+        
+        // int n = inp.nextInt();
 
-       int n = inp.nextInt();
-      
-     
-     if(n == 2){
-        System.out.println(n + "N is prime");
-     }else{
-       boolean isPrime = true;
-       for(int i= 2; i<=Math.sqrt(n); i++){
-      
-       if(n % i == 0){
-        isPrime = false;
-       }
-      }
+        //   int i = 1;   
 
-      if(isPrime == true){
-        System.out.println(n + " is prime");
-      }else{
-        System.out.println("N is not prime");
-      }
-     }
-      
-      
+        //   while(i<=n){
+        //     System.out.println(i);  //1  2  4  8  
+                                    
+        //     i = i * 2;
+
+        int count = 0;
+
+        for(int i=0; i< 10;i++){
+            System.out.println(++count);
+        }
+           
+
+
+          } 
+    
     }
-}
+
  
